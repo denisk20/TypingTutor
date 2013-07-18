da8237b526cd20bd5068175eefa21e4f0db6d56b
Denis Kniazhev <dkniazhev@spredfast.com>
2013-07-18 08:20:34 +0300

    building base structure

---
+++ typingtutor.js
@@ -0,0 +1,12 @@
+(function($){
+	var iframe;
+	//there should be 2 input elements - 1 for text and one for textarea
+	$.fn.typingtutor = function(options){
+		//todo: make check for input
+		var settings = $.extend({}, options);
+		//todo: make order independent
+		var text = this[0];
+		var textarea = this[1];
+		
+	};
+}(jQuery));
