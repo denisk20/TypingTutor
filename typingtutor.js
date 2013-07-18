9d99ba135a1688905c9fd363a56176f9318e1721
Denis Kniazhev <dkniazhev@spredfast.com>
2013-07-18 20:06:34 +0300

    working on basic highlighting

--- typingtutor.js
+++ typingtutor.js
@@ -7,6 +7,24 @@
 		//todo: make order independent
 		var text = this[0];
 		var textarea = this[1];
+		var iframe = document.createElement('iframe');
+		$(textarea).replaceWith(iframe);
 		
 	};
 }(jQuery));
+jQuery.fn.selectText = function() {
+    var range, selection;
+    if (document.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(this[0]);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(this[0]);
+//        range.setStart(this[0], 2);
+//        range.setEnd(this[0],4);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+};
