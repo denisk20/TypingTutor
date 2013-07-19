cf9fb0bddaef70179684b2e36ad6066f74a9ed63
Denis Kniazhev <dkniazhev@spredfast.com>
2013-07-19 08:06:17 +0300

    created base range operations with iframes

--- typingtutor.js
+++ typingtutor.js
@@ -2,14 +2,34 @@
 	var iframe;
 	//there should be 2 input elements - 1 for text and one for textarea
 	$.fn.typingtutor = function(options){
-		//todo: make check for input
+		//todo: validate input
 		var settings = $.extend({}, options);
 		//todo: make order independent
 		var text = this[0];
 		var textarea = this[1];
-		var iframe = document.createElement('iframe');
-		$(textarea).replaceWith(iframe);
+		var textFrame = document.createElement('iframe');
+		var typingFrame = document.createElement('iframe');
+		$(text).replaceWith(textFrame);
+		$(textarea).replaceWith(typingFrame);
+
+		$(textFrame).contents().find('body').append($(text).text());
+		$(typingFrame).contents()[0].designMode="on";
+		$(textFrame).contents()[0].designMode="on";
+		//hopefully this works even though I don't obtain selection from window, but rather from document
+		var textSelection = $(textFrame).contents()[0].getSelection();
+		//todo will need error range too
+		var textRange = $(textFrame).contents()[0].createRange();
+		//hopefully this works in all browsers too...
+		textRange.setStart($(textFrame).contents().find('body')[0].firstChild, 0);
 		
+		var textRangeEnd = 0;
+		
+		$(typingFrame).contents().find('body').keypress(function(){
+			//todo for testing only
+			textRange.setEnd($(textFrame).contents().find('body')[0].firstChild, ++textRangeEnd);
+			textSelection.removeAllRanges();
+			textSelection.addRange(textRange);
+		});
 	};
 }(jQuery));
 jQuery.fn.selectText = function() {
