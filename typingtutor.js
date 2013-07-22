fc070cdf8a20f1bd360500189daae1d2a7bc4b70
Denis Kniazhev <dkniazhev@spredfast.com>
2013-07-22 17:41:06 +0300

    multiline typing kinda works

--- typingtutor.js
+++ typingtutor.js
@@ -1,70 +1,134 @@
 (function($) {
-    //there should be 2 input elements - 1 for text and one for textarea
-    $.fn.typingtutor = function(options) {
-	//todo: validate input
-	var settings = $.extend({}, options);
-	//todo: make order independent
-	var text = this[0];
-	var textarea = this[1];
+	//there should be 2 input elements - 1 for text and one for textarea
+	$.fn.typingtutor = function(options) {
+		//todo: validate input
+		var settings = $.extend({}, options);
+		//todo: make order independent
+		var text = this[0];
+		var textarea = this[1];
+		$(textarea).css('resize', 'none');
 
-	var originalText = $(text).text();
-
-	$(text).css('font-family', '"Courier New", Courier, monospace');
-	text.innerHTML = "";
-	var letters = [];
-	for (var i = 0; i < originalText.length; i++) {
-	    var letter = $('<span>' + originalText.charAt(i) + '</span>');
-	    $(text).append(letter);
-	    letters[i] = letter;
-	}
-
-	drawCursor(0);
-
-	function drawTextBackground(position) {
-	    letters[position].css('background-color', '#C9FFE0');
-	}
-	function clearTextBackground(position) {
-	    letters[position].css('background-color', 'white');
-	}
-	function drawCursor(position) {
-	    letters[position].css('background-color', 'green');
-	}
-	function highlightError(position) {
-	    letters[position].css('background-color', 'red');
-	}
-	$(textarea).keypress(function(e) {
-	    if (e.keyCode === 8) {
-		return;
-	    }
-	    //todo: WARN: this does not take into account current line
-	    var currentTypingPosition = $(textarea).val().length;
-	    //a letter has been typed
-	    //if(originalText.charCodeAt(currentTypingPosition) === e.keyCode)
-	    if (originalText.substring(0, currentTypingPosition + 1) === $(textarea).val() + String.fromCharCode(e.which)) {
-		drawTextBackground(currentTypingPosition);
-		drawCursor(currentTypingPosition + 1);
-	    } else {
-		highlightError(currentTypingPosition);
-	    }
-	});
-	$(textarea).keydown(function(e) {
-	    if (e.keyCode === 8) {
-		//backspace - unstyle last styled character
-		var currentTypingPosition = $(textarea).val().length;
-		if (currentTypingPosition > 0) {
-		    clearTextBackground(currentTypingPosition);
-		    if (currentTypingPosition > 1) {
-			clearTextBackground(currentTypingPosition - 1);
-			if (originalText.substring(0, currentTypingPosition - 1) === $(textarea).val().substring(0, currentTypingPosition - 1)) {
-			    drawCursor(currentTypingPosition - 1);
-			} else {
-			    highlightError(currentTypingPosition - 2);
+		var currentLinePosition = 0;
+		var lines = $(text).find('p');
+		var originalTexts = [];
+		var lineLetters = [];
+		for (var i = 0; i < lines.length; i++) {
+			lines[i] = $(lines[i]);
+			var lineText = lines[i].text();
+			lines[i][0].innerHTML = '';
+			originalTexts[i] = lineText;
+			lineLetters[i] = [];
+			for (var j = 0; j < lineText.length; j++) {
+				var letter = $('<span>' + lineText.charAt(j) + '</span>');
+				lines[i].append(letter);
+				lineLetters[i][j] = letter;
 			}
-		    } else {
-			drawCursor(0);
-		    }
+			//append a whitespace to the end of each line
+			var lastLetter = $('<span>&nbsp;</span>');
+			lineLetters[i][j] = lastLetter;
+			lines[i].append(lastLetter);
+			originalTexts[i] += ' ';
 		}
-	    }
-	});
-    };
+
+		$(text).css('font-family', '"Courier New", Courier, monospace');
+
+		drawCursor(0, 0);
+
+		function drawTextBackground(line, position) {
+			lineLetters[line][position].css('background-color', '#C9FFE0');
+		}
+		function clearTextBackground(line, position) {
+			lineLetters[line][position].css('background-color', 'white');
+		}
+		function drawCursor(line, position) {
+			lineLetters[line][position].css('background-color', 'green');
+		}
+		function highlightError(line, position) {
+			lineLetters[line][position].css('background-color', 'red');
+		}
+		function getLines() {
+			var currentText = textarea.value;
+			var allLines = currentText.split(/\n/g);
+			return allLines
+		}
+		function getLinesCount() {
+			return getLines().length;
+		}
+		function getLastLine() {
+			var allLines = getLines();
+			return allLines[allLines.length - 1];
+		}
+		
+		$(textarea).keypress(function(e) {
+			if(e.keyCode === 8 || e.keyCode === 13) {
+				return;
+			}
+			//a letter has been typed
+			var lastLine = getLastLine();
+			var currentTypingPosition = lastLine.length;
+			if(currentTypingPosition > originalTexts[currentLinePosition].length - 1) {
+				return;
+			}
+		
+			if(originalTexts[currentLinePosition].substring(0, currentTypingPosition + 1) === lastLine + String.fromCharCode(e.which)) {
+				drawTextBackground(currentLinePosition, currentTypingPosition);
+				drawCursor(currentLinePosition, currentTypingPosition + 1);
+			} else {
+				highlightError(currentLinePosition, currentTypingPosition);
+			}
+		});
+		$(textarea).keydown(function(e) {
+			if(e.keyCode === 8) {
+				//backspace - unstyle last styled character
+				var lastLine = getLastLine();
+				var currentTypingPosition = lastLine.length;
+				if(currentTypingPosition > 0) {
+					if(currentTypingPosition < originalTexts[currentLinePosition].length) {
+						clearTextBackground(currentLinePosition, currentTypingPosition);
+					}
+					if(currentTypingPosition > 1) {
+						if(currentTypingPosition - 1 < originalTexts[currentLinePosition].length) {
+							clearTextBackground(currentLinePosition, currentTypingPosition - 1);
+							if(originalTexts[currentLinePosition].substring(0, currentTypingPosition - 1) === lastLine.substring(0, currentTypingPosition - 1)) {
+								drawCursor(currentLinePosition, currentTypingPosition - 1);
+							} else {
+								highlightError(currentLinePosition, currentTypingPosition - 2);
+							}
+						}
+					} else {
+						drawCursor(currentLinePosition, 0);
+					}
+				} else {
+					//we may have jumped to a previous line
+					//Remove cursor from previous line
+					if(currentLinePosition === 0){
+						//we're at the beginning of the first line
+						return;
+					}
+					clearTextBackground(currentLinePosition, 0);
+					currentLinePosition--;
+					var currentTypedLine = getLines()[currentLinePosition];
+					currentTypingPosition = currentTypedLine.length + 1; //adding one for space at the end
+					if(originalTexts[currentLinePosition].substring(0, currentTypingPosition - 1) === currentTypedLine.substring(0, currentTypingPosition - 1)) {
+						drawCursor(currentLinePosition, currentTypingPosition - 1);
+					} else {
+						highlightError(currentLinePosition, currentTypingPosition - 2);
+					}
+					
+				}
+			} else if(e.keyCode === 13) {
+				//validate current line
+				var lastLine = getLastLine();
+				if(originalTexts[currentLinePosition].trim() !== lastLine) {
+					highlightError(currentLinePosition, lastLine.length - 1);
+				} else {
+					drawTextBackground(currentLinePosition, lastLine.length);
+				}
+				currentLinePosition++;
+				drawCursor(currentLinePosition, 0);
+			}
+		});
+		
+		$(textarea).focus();
+	};
 }(jQuery));
