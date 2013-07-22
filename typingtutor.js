e8a26b20275cbd9c205b6cab7f362feb15e5d61f
Denis Kniazhev <dkniazhev@spredfast.com>
2013-07-22 07:48:13 +0300

    redesigned without iframes, added base input check

--- typingtutor.js
+++ typingtutor.js
@@ -6,35 +6,62 @@
 	//todo: make order independent
 	var text = this[0];
 	var textarea = this[1];
+
 	var originalText = $(text).text();
 	
 	text.innerHTML = "";
-	var letters = []
+	var letters = [];
 	for(var i = 0; i < originalText.length; i++){
 	    var letter = $('<span>' + originalText.charAt(i) + '</span>');
 	    $(text).append(letter);
 	    letters[i] = letter;
 	}
-	var typingFrame = document.createElement('iframe');
-	$(textarea).replaceWith(typingFrame);
 
-	var typingBody = $(typingFrame).contents().find('body');
-	var typingDocument = $(typingFrame).contents()[0];
-
-	typingDocument.designMode = "on";
-
-	typingBody.keydown(function(event) {
+	drawCursor(0);
+	
+	function drawTextBackground(position){
+	    letters[position].css('background-color', '#C9FFE0');
+	}
+	function clearTextBackground(position){
+	    letters[position].css('background-color', '');
+	}
+	function drawCursor(position){
+	    letters[position].css('background-color', 'green');
+	}
+	function highlightError(position){
+	    letters[position].css('background-color', 'red');
+	}
+	function inputCorrect(position, code){
+	    return originalText.charAt(position) === String.fromCharCode(code);
+	}
+	$(textarea).keypress(function(e) {
 	    //todo: WARN: this does not take into account current line
-	    var currentTypingPosition = typingBody.text().length;
-	    if (event.keyCode === 8) {
-		//backspace - unstyle last styled character
-
-		if (currentTypingPosition > 0) {
-		    letters[currentTypingPosition-1].css('background-color', '');
-		}
+	    var currentTypingPosition = $(textarea).val().length;
+	    //a letter has been typed
+	    //if(originalText.charCodeAt(currentTypingPosition) === e.keyCode)
+	    if(inputCorrect(currentTypingPosition, e.which)) {
+		drawTextBackground(currentTypingPosition);
+		drawCursor(currentTypingPosition + 1);
 	    } else {
-		//a letter has been typed
-		letters[currentTypingPosition].css('background-color', '#C9FFE0');
+		highlightError(currentTypingPosition);
+	    }
+	    
+	});
+	$(textarea).keydown(function(e){
+	    if(e.keyCode === 8){
+		//backspace - unstyle last styled character
+		var currentTypingPosition = $(textarea).val().length;
+		clearTextBackground(currentTypingPosition);
+		if (currentTypingPosition > 1) {
+		    clearTextBackground(currentTypingPosition - 1);
+		    if(originalText.charAt(currentTypingPosition - 2) === $(textarea).val().charAt(currentTypingPosition - 2)) {
+			drawCursor(currentTypingPosition - 1);
+		    } else {
+			highlightError(currentTypingPosition - 2);
+		    }
+		} else {
+		    drawCursor(0);
+		}
 	    }
 	});
     };
